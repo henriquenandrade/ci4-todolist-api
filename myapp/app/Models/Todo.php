8abf41d2a664498d4a921df425445b61cfd48c1e
Henrique Nascimento <henriquenascandrade@gmail.com>
2024-01-11 11:01:41 -0300

    add usetimestamp datetime

--- myapp/app/Models/Todo.php
+++ myapp/app/Models/Todo.php
@@ -14,11 +14,13 @@ class Todo extends Model
     protected $protectFields    = true;
     protected $allowedFields    = [
         'name',
-        'done'
+        'done',
+        'created_at',
+        'updated_at'
     ];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
