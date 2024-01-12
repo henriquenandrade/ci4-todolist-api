6c56b8d6743562a7c925c524836f66e7add6aa4a
henriquenandrade <henriquenascandrade@gmail.com>
2024-01-12 12:58:04 -0300

    router fix

--- myapp/app/Config/Routes.php
+++ myapp/app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->resource('todos', ['controller' => 'TodoController']);
+$routes->resource('todos', '');
+
+// $routes->get('/todos', 'TodoController::index', ['name' => 'index']);
+// $routes->get('/todos/(:num)', 'TodoController::show/$1', ['name' => 'show']);
+// $routes->post('/todos', 'TodoController::store', ['name' => 'store']);
+// $routes->put('/todos/(:num)', 'TodoController::update/$1', ['name' => 'update']);
+// $routes->delete('/todos/(:num)', 'TodoController::delete/$1', ['name' => 'delete']);
